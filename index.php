077a62d9e6301eb6ff9ce6a6d7bfa2a65c9adaf7
GhozyMHM <gliozghozy@gmail.com>
2020-06-09 15:36:08 +0700

    Update Banner

--- index.php
+++ index.php
@@ -28,7 +28,9 @@ include 'dbconnection.php';
             <!--Navigation Bar-->
             <nav class="navbar navbar-expand-lg navbar-dark fixed-top pb-0 pt-0 navbar-holy">
                 <a class="navbar-brand" href="index.html"><img class="icon" src="Style/img/logo-square.png"></a>
-                <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                <button class="navbar-toggler" type="button" data-toggle="collapse"
+                    data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false"
+                    aria-label="Toggle navigation">
                     <span class="navbar-toggler-icon"></span>
                 </button>
                 <div class="collapse navbar-collapse" id="navbarSupportedContent">
@@ -54,30 +56,30 @@ include 'dbconnection.php';
             <!--Navigation Bar End-->
 
             <!--Carousel-->
-            <div id="carouselExampleIndicators" class="carousel slide" data-ride="carousel">
+            <div id="carousel-indicators" class="carousel slide" data-ride="carousel">
                 <ol class="carousel-indicators">
-                    <li data-target="#carouselExampleIndicators" data-slide-to="0" class="active"></li>
-                    <li data-target="#carouselExampleIndicators" data-slide-to="1"></li>
-                    <li data-target="#carouselExampleIndicators" data-slide-to="2"></li>
+                    <li data-target="#carousel-indicators" data-slide-to="0" class="active"></li>
+                    <li data-target="#carousel-indicators" data-slide-to="1"></li>
+                    <li data-target="#carousel-indicators" data-slide-to="2"></li>
                 </ol>
 
                 <div class="carousel-inner">
                     <div class="carousel-item active">
-                        <img src="Style/img/bannerHLDM1.jpg" class="d-block w-100" alt="...">
+                        <img src="Style/img/bannerHLDM1.jpg" class="d-block w-100" alt="Holydamn-Banner">
                     </div>
                     <div class="carousel-item">
-                        <img src="Style/img/bannerHD2.jpg" class="d-block w-100" alt="...">
+                        <img src="Style/img/bannerHLDM2.jpg" class="d-block w-100" alt="Holydamn-Banner">
                     </div>
                     <div class="carousel-item">
-                        <img src="Style/img/bannerHD3.jpg" class="d-block w-100" alt="...">
+                        <img src="Style/img/bannerHLDM3.jpg" class="d-block w-100" alt="Holydamn-Banner">
                     </div>
                 </div>
 
-                <a class="carousel-control-prev" href="#carouselExampleIndicators" role="button" data-slide="prev">
+                <a class="carousel-control-prev" href="#carousel-indicators" role="button" data-slide="prev">
                     <span class="carousel-control-prev-icon" aria-hidden="true"></span>
                     <span class="sr-only">Previous</span>
                 </a>
-                <a class="carousel-control-next" href="#carouselExampleIndicators" role="button" data-slide="next">
+                <a class="carousel-control-next" href="#carousel-indicators" role="button" data-slide="next">
                     <span class="carousel-control-next-icon" aria-hidden="true"></span>
                     <span class="sr-only">Next</span>
                 </a>
@@ -117,7 +119,8 @@ include 'dbconnection.php';
             <div class="jumbotron jumbotron-fluid">
                 <div class="row">
                     <div class="col text-center">
-                        <button class="btn btn-dark">ORDER</button>
+                        <a href="https://linktr.ee/Holydamn" target="_blank"><button
+                                class="btn btn-dark">ORDER</button></a>
                     </div>
                 </div>
             </div>
@@ -268,9 +271,9 @@ include 'dbconnection.php';
                         </div>
                         <div class="col-md-3 col-sm-12">
                             <div class="gallery-item">
-                                <img src="Style/img/Pict1.jpeg" alt="gallery 7">
+                                <img src="Style/img/Pict6.jpeg" alt="gallery 7">
                                 <div class="overlay">
-                                    <a href="Style/img/Pict1.jpeg" data-rel="lightbox" class="fa fa-arrows-alt"></a>
+                                    <a href="Style/img/Pict6.jpeg" data-rel="lightbox" class="fa fa-arrows-alt"></a>
                                 </div>
                             </div>
                         </div>
@@ -289,7 +292,9 @@ include 'dbconnection.php';
                 <div class="card card-wrap mb-3">
                     <div class="card-header card-accord" id="headingOne">
                         <h2 class="mb-0">
-                            <button id="title-card" class="btn btn-link btn-block text-left collapsed button-title" type="button" data-toggle="collapse" data-target="#collapseOne" aria-expanded="true" aria-controls="collapseOne">
+                            <button id="title-card" class="btn btn-link btn-block text-left collapsed button-title"
+                                type="button" data-toggle="collapse" data-target="#collapseOne" aria-expanded="true"
+                                aria-controls="collapseOne">
                                 <i class="fas fa-chevron-down fa-lg mr-3"></i>Holydamn Brand
                             </button>
                         </h2>
@@ -309,7 +314,9 @@ include 'dbconnection.php';
                 <div class="card card-wrap mb-3">
                     <div class="card-header card-accord" id="headingTwo">
                         <h2 class="mb-0">
-                            <button id="title-card" class="btn btn-link btn-block text-left collapsed button-title" type="button" data-toggle="collapse" data-target="#collapseTwo" aria-expanded="false" aria-controls="collapseTwo">
+                            <button id="title-card" class="btn btn-link btn-block text-left collapsed button-title"
+                                type="button" data-toggle="collapse" data-target="#collapseTwo" aria-expanded="false"
+                                aria-controls="collapseTwo">
                                 <i class="fas fa-chevron-down fa-lg mr-3"></i>Our Product
                             </button>
                         </h2>
@@ -331,13 +338,16 @@ include 'dbconnection.php';
                 <div class="card card-wrap">
                     <div class="card-header card-accord" id="headingThree">
                         <h2 class="mb-0">
-                            <button id="title-card" class="btn btn-link btn-block text-left collapsed button-title" type="button" data-toggle="collapse" data-target="#collapseThree" aria-expanded="false" aria-controls="collapseThree">
+                            <button id="title-card" class="btn btn-link btn-block text-left collapsed button-title"
+                                type="button" data-toggle="collapse" data-target="#collapseThree" aria-expanded="false"
+                                aria-controls="collapseThree">
                                 <i class="fas fa-chevron-down fa-lg mr-3"></i>This Website
                             </button>
                         </h2>
                     </div>
 
-                    <div id="collapseThree" class="collapse" aria-labelledby="headingThree" data-parent="#accordionExample">
+                    <div id="collapseThree" class="collapse" aria-labelledby="headingThree"
+                        data-parent="#accordionExample">
                         <div class="card-body body-card">
                             Welcome to the landing page of HOLYDAMN. This is the right place
                             to upgrade your style with our product. You can see our catalog and Information
@@ -361,11 +371,13 @@ include 'dbconnection.php';
                                     <div class="col-lg-12">
                                         <input spellcheck="false" type="text" placeholder="Full Name" name="fullname">
 
-                                        <input spellcheck="false" type="email" placeholder="youremail@example.com" name="email">
+                                        <input spellcheck="false" type="email" placeholder="youremail@example.com"
+                                            name="email">
 
                                         <input spellcheck="false" type="text" placeholder="Subject" name="subject">
 
-                                        <textarea spellcheck="false" placeholder="Your Message" name="message"></textarea>
+                                        <textarea spellcheck="false" placeholder="Your Message"
+                                            name="message"></textarea>
                                     </div>
 
                                     <div class="col-lg-12 text-left">
@@ -441,11 +453,14 @@ include 'dbconnection.php';
 
     </div>
 
-    <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous">
+    <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js"
+        integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous">
     </script>
-    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous">
+    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js"
+        integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous">
     </script>
-    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/js/bootstrap.min.js" integrity="sha384-OgVRvuATP1z7JjHLkuOU7Xw704+h835Lr+6QL9UvYjZE3Ipu6Tp75j7Bh/kR0JKI" crossorigin="anonymous">
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/js/bootstrap.min.js"
+        integrity="sha384-OgVRvuATP1z7JjHLkuOU7Xw704+h835Lr+6QL9UvYjZE3Ipu6Tp75j7Bh/kR0JKI" crossorigin="anonymous">
     </script>
     <script src="Style/owl.carousel.min.js"></script>
     <script src="JS/swiper.min.js"></script>
